e0ce4c296bd36a45bad353535bbce1ecdb13ed1b
奉伟 <landwind1180@gmail.com>
2019-09-24 16:53:25 +0800

    添加部署配置

---
+++ obj/Release/netcoreapp3.0/win-x86/PubTmp/Out/ClientApp/build/precache-manifest.2076b9515977cdd0ef02686e42b9ebda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47228cf5c04a209d2868c7e2e86b2162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b095a912109438215d57",
+    "url": "/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "bea102c12342a98179ea",
+    "url": "/static/css/main.6636e503.chunk.css"
+  },
+  {
+    "revision": "b095a912109438215d57",
+    "url": "/static/js/2.072acdf6.chunk.js"
+  },
+  {
+    "revision": "bea102c12342a98179ea",
+    "url": "/static/js/main.399e5fc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
